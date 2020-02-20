eb27192bc0d63b67c1ef5e77d9e12e530f2748af
lgrellie <leonard.grellier@gmail.com>
2020-02-20 20:20:35 +0100

    Pushit

--- input.py
+++ input.py
@@ -28,5 +28,22 @@ def scoring(scores, is_scanned):
         sum += scores[i] * is_scanned[i]
     return sum
 
+def filter_signups(lib_order, ):
+
+def find_best_book(is_scanned):
+    best = -1
+    max_score = 0
+    for b in range(B):
+        if not is_scanned[b] and scores[b] > max_score:
+            best = b
+            max_score = scores[b]
+    return best
+
+def compute_lib_score(is_scanned, sign_ups, signed_up):
+    begin_time = 0
+    for i in range(signed_up):
+        begin_time += libraries[sign_ups[i]][0][1]
+    if begin_time < D:
+        while 
 
 print(scoring(scores, is_scanned))
