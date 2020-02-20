ef702628017533704f508d35e318a816e788fdae
lgrellie <leonard.grellier@gmail.com>
2020-02-20 20:26:54 +0100

    Stsah

--- input.py
+++ input.py
@@ -28,12 +28,12 @@ def scoring(scores, is_scanned):
         sum += scores[i] * is_scanned[i]
     return sum
 
-def filter_signups(lib_order, ):
+def filter_signups():
 
-def find_best_book(is_scanned):
+def find_best_book(is_scanned, books):
     best = -1
     max_score = 0
-    for b in range(B):
+    for b in books:
         if not is_scanned[b] and scores[b] > max_score:
             best = b
             max_score = scores[b]
@@ -41,9 +41,17 @@ def find_best_book(is_scanned):
 
 def compute_lib_score(is_scanned, sign_ups, signed_up):
     begin_time = 0
+    lib_score = 0
     for i in range(signed_up):
         begin_time += libraries[sign_ups[i]][0][1]
     if begin_time < D:
-        while 
+
+    return lib_score
+
+def find_best_lib(is_scanned, sign_ups, signed_up):
+    best_= -1
+    max_score = 0
+    for l in libraries:
+
 
 print(scoring(scores, is_scanned))
